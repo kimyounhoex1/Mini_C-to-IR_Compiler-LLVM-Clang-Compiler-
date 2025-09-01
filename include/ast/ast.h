c43b76506107dfb90df29d3f2bc3184b97ac37f6
kimyounhoex1 <dordyd11@gmail.com>
2025-09-02 01:18:24 +0900

    feat(AstNode): 노드 타입 추가

--- include/ast/ast.h
+++ include/ast/ast.h
@@ -6,6 +6,7 @@ class Visitor;
 
 struct AstNode {
     virtual ~AstNode() = default;
+    virtual std::string getType() const { return "unknown"; }
     virtual void accept(class Visitor& v) = 0; // 방문자 받아들이기
 };
 
@@ -40,6 +41,32 @@ struct Block : AstNode {
     std::vector<AstNode*> stmts;
     Block(std::vector<AstNode*> s) {
         stmts = s;
+    }
+    void accept(Visitor& v) override;
+};
+
+struct Number : AstNode {
+    int value;
+    Number(int v) { value = v; };
+    std::string getType() const override { return "int"; }
+    void accept(Visitor &v) override;
+};
+
+struct Boolean : AstNode {
+    bool value;
+    Boolean(bool v) { value = v; };
+    std::string getType() const override { return "bool"; }
+    void accept(Visitor &v) override;
+};
+
+struct BinaryOp : AstNode {
+    std:: string op;
+    AstNode* lhs;
+    AstNode* rhs;
+    BinaryOp(std::string o, AstNode* l, AstNode* r) {
+        op = std::move(o);
+        lhs = std::move(l);
+        rhs = std::move(r);
     }
     void accept(Visitor& v) override;
 };
